4e6daa76d5e2a47ca88dce173216e654f20e27fd
Taufik <taufikhidayat0032@gmail.com>
2025-08-29 21:59:26 +0700

    1.first set up finished

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,16 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    ignorePatterns: ["src/generated/**/**"],
+  }),
 ];
 
 export default eslintConfig;
